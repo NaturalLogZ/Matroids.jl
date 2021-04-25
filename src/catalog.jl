8520d986161db1d5f9aa3e9731e015262cdf6215
Ricky Shapley <rls499@cornell.edu>
2021-04-25 02:42:24 -0400

    Add a few more matroids to catalog

--- src/catalog.jl
+++ src/catalog.jl
@@ -6,31 +6,60 @@ lots of work.
 module Catalog
 
 using Matroids
+import AbstractAlgebra
+import Nemo
 
-# TODO: redefine via matrix or smth...
-# But for now, this works I think.
+
+# TODO: compare these two definitions of fano when is_isomorphic is done
+
+# function Fano()
+#     gs = collect("abcdefg")
+#     nbs = [
+#               ['a','d','b'],
+#               ['b','e','c'],
+#               ['a','f','c'],
+#               ['a','g','e'],
+#               ['d','g','c'],
+#               ['b','g','f'],
+#               ['d','e','f']
+#           ]
+# 
+#     return BasisMatroid(groundset=gs, nonbases=nbs)
+# end
 
 function Fano()
-    gs = ['a','b','c','d','e','f','g']
-    nbs = [
-              ['a','d','b'],
-              ['b','e','c'],
-              ['a','f','c'],
-              ['a','g','e'],
-              ['d','g','c'],
-              ['b','g','f'],
-              ['d','e','f']
-          ]
-
-    return BasisMatroid(groundset=gs, nonbases=nbs)
+    F = AbstractAlgebra.GF(2)
+    mtx = AbstractAlgebra.matrix(F, [1 0 0 0 1 1 1;
+                                     0 1 0 1 0 1 1;
+                                     0 0 1 1 1 0 1])
+    return LinearMatroid(mtx, groundset=collect("abcdefg"))
 end
 
+function Q6()
+    F, x = Nemo.FiniteField(2, 2, "x")
+    mtx = AbstractAlgebra.matrix(F, [1 0 0 1 0 1;
+                                     0 1 0 1 1 x;
+                                     0 0 1 0 1 1])
 
+    return LinearMatroid(mtx, groundset=collect("abcdef"))
+end
 
+function R6()
+    F = AbstractAlgebra.GF(3)
+    mtx = AbstractAlgebra.matrix(F, [1 0 0 1 1 1;
+                                     0 1 0 1 2 1;
+                                     0 0 1 1 0 2])
 
+    return LinearMatroid(mtx, groundset=collect("abcdef"))
+end
 
-
-
+function NonFano()
+    F = AbstractAlgebra.GF(3)
+    mtx = AbstractAlgebra.matrix(F, [1 0 0 0 1 1 1;
+                                     0 1 0 1 0 1 1;
+                                     0 0 1 1 1 0 1])
+    return LinearMatroid(mtx, groundset=collect("abcdefg"))
+end
 
 
 end
